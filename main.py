b3513631ed76ebb9ea6520cfd1e561091fb20412
Tao Peng <tpeng@ufl.edu>
2023-03-20 16:05:40 -0400

    Newest edition

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# menu display
+# menu
 def print_menu():
     print('Menu\n------------- \n1. Encode\n2. Decode\n3. Quit\n')
     option = input('Please enter an option: ')
@@ -9,9 +9,9 @@ def encode(password):
     result = ''
     for i in password:
         current = int(i)
-        current -= 3
-        if current < 0:
-            current += 10
+        current += 3
+        if current > 9:
+            current -= 10
         result += str(current)
     return result
 
@@ -34,19 +34,17 @@ if __name__ == '__main__':
                 print('Your password has been encoded and stored!\n')
                 type = 'encode'
             else:
-                dec_password = encode(password)
-                print(f'The decoded password is {dec_password}, and the original password is {password}.\n')
+                print(f'The encoded password is {password}, and the original password is {decode(password)}.\n')
                 password_stored = False
-        # decode dispaly
-        if option == '2':
+        # decode display
+        elif option == '2':
             if not password_stored or type != 'encode':
                 password = input('Please enter your password to decode: ')
                 password_stored = True
                 print('Your password has been decoded and stored!\n')
                 type = 'decode'
             else:
-                enc_password = decode(password)
-                print(f'The encoded password is {enc_password}, and the original password is {password}.\n')
+                print(f'The encoded password is {encode(password)}, and the original password is {password}.\n')
                 password_stored = False
         # new start
         option = print_menu()
