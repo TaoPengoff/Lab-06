e393035701761023981dcc984435e9b0bb614e62
yixuanyang123 <yixuanyang@ufl.edu>
2023-03-21 20:33:05 -0400

    added the decode() function

--- main.py
+++ main.py
@@ -17,7 +17,14 @@ def encode(password):
 
 # decode function
 def decode(password):
-    pass
+    result = ''
+    for i in password:
+        current = int(i)
+        current -= 3
+        if current > 9:
+            current += 10
+        result += str(current)
+    return result
 
 if __name__ == '__main__':
     # default
